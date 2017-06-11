cb4d7327a1d27cc81d2666070453b43b43c28025
James Summers <jamesnsummers@gmail.com>
2017-06-10 19:19:32 -0500

    added ProjectsIndex Controller for FE

---
+++ public/scripts/controllers/ProjectsIndexController.js
@@ -0,0 +1,57 @@
+angular
+  .module('portfolio')
+  .controller('ProjectsIndexController', ProjectsIndexController);
+
+ProjectsIndexController.$inject = ['$http'];
+
+function ProjectsIndexController ($http) {
+  var vm = this;
+
+  $http({
+    method: 'GET',
+    url: '/api/projects'
+  }).then(function successCallback(response) {
+    vm.projects = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  vm.createProject = function () {
+    $http({
+      method: 'POST',
+      url: '/api/projects',
+      data: vm.newProject,
+    }).then(function successCallback(response) {
+      vm.projects.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
+  vm.editProject = function (film) {
+    $http({
+      method: 'PUT',
+      url: '/api/projects/'+project._id,
+      data: project
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+  vm.deleteProject = function (project) {
+    $http({
+      method: 'DELETE',
+      url: '/api/projects/'+ project._id
+    }).then(function successCallback(json) {
+      var index = vm.projects.indexOf(project);
+      vm.projects.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+
+
+
+}
