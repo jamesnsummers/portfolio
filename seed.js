60eeb2d0cfcf914e26ed2ab846c59db8875c649d
James Neill Summers <jamesneillsummers@MacBook-Pro-5.local>
2019-04-16 23:40:31 -0500

    reorder projects and remove one

--- seed.js
+++ seed.js
@@ -5,6 +5,20 @@ var db = require('./models');
 
 var projectList = [];
   projectList.push({
+    title: "A/B Testing w/ React",
+    dateCompleted: "05/15/2018",
+    url: "https://github.com/jamesnsummers/ab-testing-with-react",
+    descrip: "This app showcases a lightweight approach to implementing A/B tests on a React app.",
+    image: "/images/ab-testing-react.png"
+  });
+  projectList.push({
+    title: "Personal Portfolio",
+    dateCompleted: "07/01/2017",
+    url: "http://www.jamesnsummers.com",
+    descrip: "I built this portfolio using AngularJS, HTML, CSS, Javascript, jQuery, Mongoose/MongoDB, and Express. I enjoy meta things, thus this.",
+    image: "/images/JnS.png"
+  });
+  projectList.push({
     title: "Tic Tac Toe",
     dateCompleted: "05/07/2017",
     url: "http://jim-jac-joe.bitballoon.com/",
@@ -12,19 +26,19 @@ var projectList = [];
     image: "/images/tic-tac-toe.png"
   });
   projectList.push({
-    title: "Geoquakes",
-    dateCompleted: "05/14/2017",
-    url: "https://github.com/jamesnsummers/geoquakes.git",
-    descrip: "An app built using jQuery's ajax() function to make calls to a third party API, Google Maps. Uses live data from the USGS (United States Geological Survey), specifically a data set showing significant earthquakes (M4.0 or greater) from the past week.",
-    image: "/images/geoquakes.png"
+    title: "Marathonr",
+    dateCompleted: "07/14/2017",
+    url: "https://github.com/jamesnsummers/Marathonr-App",
+    descrip: "The Marathonr app builds movie marathons. Users enter their location data, pick a date, and select the movies they want to see, and Marathonr does the rest! This project was created using Python, Django, SQLite, HTML, and CSS.",
+    image: "/images/countdown.gif"
   });
-  projectList.push({
-    title: "Choose Your Own Adventure",
-    dateCompleted: "05/08/2017",
-    url: "https://github.com/jamesnsummers/js_adventure.git",
-    descrip: "Go down the rabbit hole in this game built with pure javascript",
-    image: "/images/cyoa.png"
-  });
+  // projectList.push({
+  //   title: "Geoquakes",
+  //   dateCompleted: "05/14/2017",
+  //   url: "https://github.com/jamesnsummers/geoquakes.git",
+  //   descrip: "An app built using jQuery's ajax() function to make calls to a third party API, Google Maps. Uses live data from the USGS (United States Geological Survey), specifically a data set showing significant earthquakes (M4.0 or greater) from the past week.",
+  //   image: "/images/geoquakes.png"
+  // });
   projectList.push({
     title: "TacOMG",
     dateCompleted: "06/02/2017",
@@ -40,25 +54,11 @@ var projectList = [];
     image: "/images/timeshare.png"
   });
   projectList.push({
-    title: "Marathonr",
-    dateCompleted: "07/14/2017",
-    url: "https://github.com/jamesnsummers/Marathonr-App",
-    descrip: "The Marathonr app builds movie marathons. Users enter their location data, pick a date, and select the movies they want to see, and Marathonr does the rest! This project was created using Python, Django, SQLite, HTML, and CSS.",
-    image: "/images/countdown.gif"
-  });
-  projectList.push({
-    title: "Personal Portfolio",
-    dateCompleted: "07/01/2017",
-    url: "http://www.jamesnsummers.com",
-    descrip: "I built this portfolio using AngularJS, HTML, CSS, Javascript, jQuery, Mongoose/MongoDB, and Express. I enjoy meta things, thus this.",
-    image: "/images/JnS.png"
-  });
-  projectList.push({
-    title: "A/B Testing w/ React",
-    dateCompleted: "05/15/2018",
-    url: "https://github.com/jamesnsummers/ab-testing-with-react",
-    descrip: "This app showcases a lightweight approach to implementing A/B tests on a React app.",
-    image: "/images/ab-testing-react.png"
+    title: "Choose Your Own Adventure",
+    dateCompleted: "05/08/2017",
+    url: "https://github.com/jamesnsummers/js_adventure.git",
+    descrip: "Go down the rabbit hole in this game built with pure javascript",
+    image: "/images/cyoa.png"
   });
 
 db.Project.remove(function(err, succ){
