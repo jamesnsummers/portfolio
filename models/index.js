fe608548602b14a31e3b360f6b64e12bf4803367
James Summers <jamesnsummers@gmail.com>
2019-04-20 15:58:22 -0500

    uncomment code

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-// var mongoose = require("mongoose");
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio");
-//
-// var Project = require('./project');
-//
-// module.exports.Project = Project;
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio");
+
+var Project = require('./project');
+
+module.exports.Project = Project;
