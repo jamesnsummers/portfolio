85d3b7b2790a76539ae890545fc17bcfde650918
James Summers <jamesnsummers@gmail.com>
2020-01-09 19:24:34 -0600

    add cake study and update mongo shit

--- seed.js
+++ seed.js
@@ -10,7 +10,7 @@ var projectList = [];
     url: "http://jim-jac-joe.bitballoon.com/",
     descrip: "A game of tic-tac-toe built using HTML, CSS, and Javascript.",
     image: "/images/tic-tac-toe.png",
-    order: 7
+    order: 8
   });
   projectList.push({
     title: "TacOMG",
@@ -20,7 +20,7 @@ var projectList = [];
               It allows users to create their ideal taco and populate a gallery of tacos. In this gallery,
               they can vote for their favorite tacos and see how many votes each taco has.`,
     image: "/images/tacomg.png",
-    order: 6
+    order: 7
   });
   projectList.push({
     title: "TimeShare",
@@ -29,7 +29,7 @@ var projectList = [];
     descrip: `TimeShare is an app built using the MERN (MongoDB, Express, ReactJS, and Node.js) stack and Firebase for auth.
               It allows people to post content that captures meaningful moments in their lives.`,
     image: "/images/timeshare.png",
-    order: 5
+    order: 6
   });
   projectList.push({
     title: "Personal Portfolio",
@@ -37,7 +37,7 @@ var projectList = [];
     url: "http://www.jamesnsummers.com",
     descrip: "I built this portfolio using AngularJS, HTML, CSS, Javascript, jQuery, Mongoose/MongoDB, and Express. I enjoy meta things, thus this.",
     image: "/images/JnS.png",
-    order: 4
+    order: 5
   });
   projectList.push({
     title: "Marathonr",
@@ -47,7 +47,7 @@ var projectList = [];
               Users enter their location data, pick a date, and select the movies they want to see,
               and Marathonr does the rest! This project was created using Python, Django, SQLite, HTML, and CSS.`,
     image: "/images/countdown.gif",
-    order: 3
+    order: 4
   });
   projectList.push({
     title: "Matthew Genitempo - Photography",
@@ -59,7 +59,7 @@ var projectList = [];
               using their developer tools, and writing custom code, I finished the front-end updates, and we
               launched the site on February 26, 2018.`,
     image: "/images/matthew_genitempo.png",
-    order: 2
+    order: 3
   });
   projectList.push({
     title: "A/B Testing w/ React",
@@ -67,6 +67,14 @@ var projectList = [];
     url: "https://github.com/jamesnsummers/ab-testing-with-react",
     descrip: "This app showcases a lightweight approach to implementing A/B tests on a React app.",
     image: "/images/a-b_testing_w_react.png",
+    order: 2
+  });
+  projectList.push({
+    title: "The Cake Study",
+    dateCompleted: "12/20/2019",
+    url: "https://www.thecakestudy.com/",
+    descrip: "A throwback website to host case study materials for award entries.",
+    image: "/images/cake_study.png",
     order: 1
   });
 
